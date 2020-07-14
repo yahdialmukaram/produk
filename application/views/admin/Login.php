5edaec4102155400ccabf15e34e5a70ad334b6f8
yahdi almukaram <yahdialmukaram03@gmail.com>
2020-07-14 19:27:04 +0700

    required

--- application/views/admin/Login.php
+++ application/views/admin/Login.php
@@ -36,7 +36,7 @@
 						</div>
 						<?php endif; ?>
         <div class="input-group mb-3">
-          <input type="text" name="username" class="form-control" placeholder="Masukan Username">
+          <input type="text" name="username" class="form-control" required placeholder="Masukan Username">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -44,7 +44,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" name="password" class="form-control" placeholder="Masukan Password">
+          <input type="password" name="password" class="form-control" required placeholder="Masukan Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
